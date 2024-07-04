6071304cde6a483ded18a8b1fdcf4769e7d136d6
OleksiiP <alex1204park@gmail.com>
2024-07-04 12:18:23 +0300

    chord(fix: server.js)

--- src/server.js
+++ src/server.js
@@ -17,6 +17,7 @@ const setupServer = async () => {
   const app = express();
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use(
     express.json({
       type: ['application/json', 'application/vnd.api+json'],
@@ -36,9 +37,6 @@ const setupServer = async () => {
   app.use('*', notFoundMiddleware);
 
   app.use(errorHandlerMiddleware);
-
-  app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
